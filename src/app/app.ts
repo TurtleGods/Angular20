a94658e860ecd300780efff6c183a44748340b6d
Turtlegods <strollist@gmail.com>
2025-07-08 16:18:49 +0800

    [feat] section 3 加入了新的daisyUI, tailwind style

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client2';
+export class App implements OnInit{
+
+  protected title = 'Dating App';
+  private http = inject(HttpClient);
+  protected members = signal<any>([]);
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+    }
+
+  async getMembers(){
+    try{
+      return  lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+    }catch(error){
+      console.log(error);
+      throw error;
+    }
+  }
+  // setCurrentUser() {
+  //   const userString = localStorage.getItem('user');
+  //   if(!userString) return;
+  //   const user = JSON.parse(userString);
+  //   this.accountService.currentUser.set(user);
+  // }
 }
