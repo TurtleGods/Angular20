d59bd097e6263c34e800eb95d3e2f1e71fa2ef26
Turtlegods <strollist@gmail.com>
2025-07-09 17:10:19 +0800

    feat 加入home跟login設定

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,26 +3,36 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from '../features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-
+export class App implements OnInit {
+  private accountSerivce = inject(AccountService);
   protected title = 'Dating App';
   private http = inject(HttpClient);
   protected members = signal<any>([]);
   async ngOnInit() {
-    this.members.set(await this.getMembers())
-    }
+    this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
 
-  async getMembers(){
-    try{
-      return  lastValueFrom(this.http.get('https://localhost:5001/api/members'))
-    }catch(error){
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountSerivce.currentUser.set(user);
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+    } catch (error) {
       console.log(error);
       throw error;
     }
