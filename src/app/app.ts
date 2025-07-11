b91a2562583793fde34acd1e973dcfdaf9aa8758
Turtlegods <strollist@gmail.com>
2025-07-11 23:58:37 +0800

    [feat] 加入app initializer，加入了頁面切換淡入淡出功能

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/User';
 
 @Component({
   selector: 'app-root',
@@ -12,36 +8,7 @@ import { User } from '../types/User';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+export class App {
   protected router = inject(Router);
-  private accountSerivce = inject(AccountService);
-  protected title = 'Dating App';
-  private http = inject(HttpClient);
-  protected members = signal<User[]>([]);
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
 
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountSerivce.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'))
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
-  // setCurrentUser() {
-  //   const userString = localStorage.getItem('user');
-  //   if(!userString) return;
-  //   const user = JSON.parse(userString);
-  //   this.accountService.currentUser.set(user);
-  // }
 }
--- src/core/services/init-service.ts
+++ src/core/services/init-service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AccountService } from './account-service';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
+  private accountService = inject(AccountService);
 
-  constructor() { }
+  init() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return of(null);
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+
+    return of(null);
+  }
+
+
 }
