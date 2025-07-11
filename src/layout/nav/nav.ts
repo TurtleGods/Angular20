d797234c926b695e10b2358d4b8e5295938dfcbd
Turtlegods <strollist@gmail.com>
2025-07-11 15:30:05 +0800

    manual 加入toast, 使用javascript去避免angular跟套件改版不相容問題

--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,29 +1,35 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule,RouterLink,RouterLinkActive],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected accountService = inject(AccountService);
-  protected creds:any={}
+  private toast = inject(ToastService);
+  private router = inject(Router);
+  protected creds: any = {}
 
-  login(){
+  login() {
     this.accountService.login(this.creds).subscribe({
-      next:result=>{
-        console.log(result);
-        this.creds={};
+      next: result => {
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
+        this.creds = {};
       },
-      error:error=>alert(error.message),
+      error: error => {
+        this.toast.error(error.error);
+      },
     });
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
   }
 
