429d80312e783cd852ec698ecfb7d7fd440df5a8
Turtlegods <strollist@gmail.com>
2025-07-15 13:58:59 +0800

    使用Resolver 去取得member detail功能

--- src/features/members/member-detailed/member-detailed.ts
+++ src/features/members/member-detailed/member-detailed.ts
@@ -1,25 +1,24 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
-import { filter, Observable } from 'rxjs';
+import { filter } from 'rxjs';
 import { Member } from '../../../types/memer';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe,RouterLink,RouterLinkActive,RouterOutlet],
+  imports: [RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css'
 })
 export class MemberDetailed implements OnInit{
-  private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
-  protected member$? : Observable<Member>;
+  protected member= signal<Member |undefined>(undefined);
   private router = inject(Router);
   protected title = signal<string|undefined>('Profile');
   
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+      next:data=>this.member.set(data['member'])
+    })
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -32,13 +31,5 @@ export class MemberDetailed implements OnInit{
       }
     )
   }
-
-  loadMember(){
-    const id = this.route.snapshot.paramMap.get('id'); //according route to get the parameter
-    if(!id) return;
-    return this.memberService.getMember(id);
-    
-  }
-
 
 }
