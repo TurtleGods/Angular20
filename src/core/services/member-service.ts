396091c15aceb9130b8683e42664a040dc850920
Turtlegods <strollist@gmail.com>
2025-07-15 15:46:47 +0800

    建立photo跟使用date pipe

--- src/core/services/member-service.ts
+++ src/core/services/member-service.ts
@@ -1,20 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { Member } from '../../types/memer';
+import { Member, Photo } from '../../types/memer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private baseUrl=environment.apiUrl;
+  private baseUrl = environment.apiUrl;
 
-  getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+'members');
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
-  getMember(id:string){
-    return this.http.get<Member>(this.baseUrl+'members/'+id);
+  getMember(id: string) {
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
+  }
+
+  getMemberPhotos(id: string) {
+    return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
   }
 }
--- src/features/members/member-photos/member-photos.ts
+++ src/features/members/member-photos/member-photos.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MemberService } from '../../../core/services/member-service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Photo } from '../../../types/memer';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
 export class MemberPhotos {
+  private memberService = inject(MemberService);
+  private route = inject(ActivatedRoute);
+  protected photo$?: Observable<Photo[]>;
+  constructor() {
+    const memberId = this.route.parent?.snapshot.paramMap.get('id');
+    if (memberId) {
+      this.photo$ = this.memberService.getMemberPhotos(memberId);
+    }
+  }
 
+  get photoMocks(){
+    return Array.from({length:20},(_,i)=>({
+      url:'/user.png'
+    }))
+  }
 }
