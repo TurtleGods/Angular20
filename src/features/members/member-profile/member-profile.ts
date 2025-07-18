dd56783630d266b4015666fc9a1e47fc36070aa6
Turtlegods <strollist@gmail.com>
2025-07-18 23:53:33 +0800

    update memberService, set member as Signal

--- src/features/members/member-profile/member-profile.ts
+++ src/features/members/member-profile/member-profile.ts
@@ -8,54 +8,54 @@ import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe,FormsModule],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile implements OnInit, OnDestroy{
-  @ViewChild('editForm') editForm?:NgForm;
-  @HostListener('window:beforeunload',['$event']) notify($event:BeforeUnloadEvent){
-    if(this.editForm?.dirty){
+export class MemberProfile implements OnInit, OnDestroy {
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event: BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
       $event.preventDefault();
     }
   }
   protected memberService = inject(MemberService);
   private toast = inject(ToastService);
-  private route = inject(ActivatedRoute)
-  protected member = signal<Member|undefined>(undefined);
-  protected editableMember:EditableMember={
-    displayName:'',
-    description:'',
-    city:'',
-    country:'',
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: '',
   }
-  constructor(){
+  constructor() {
 
   }
   ngOnInit(): void {
-    this.route.parent?.data.subscribe(data=>{
-      this.member.set(data["member"]);
-    })
-    this.editableMember={
-      displayName:this.member()?.displayName||'',
-      description:this.member()?.description||'',
-      city:this.member()?.city||'',
-      country:this.member()?.country||'',
-      
+
+    this.editableMember = {
+      displayName: this.memberService.member()?.displayName || '',
+      description: this.memberService.member()?.description || '',
+      city: this.memberService.member()?.city || '',
+      country: this.memberService.member()?.country || '',
+
     }
   }
-  
-  updateProfile(){
-    if(!this.member()) return;
-    const updateMember ={...this.member(),...this.editableMember};
-    console.log(updateMember);
-    this.toast.success("Profile updated success");
-    this.memberService.editMode.set(false);
-    
+
+  updateProfile() {
+    if (!this.memberService.member()) return;
+    const updateMember = { ...this.memberService.member(), ...this.editableMember };
+    this.memberService.updateMember(this.editableMember).subscribe({
+      next: () => {
+        this.toast.success("Profile updated successfully");
+        this.memberService.editMode.set(false);
+        this.memberService.member.set(updateMember as Member);
+        this.editForm?.reset(updateMember);
+      }
+    });
   }
   ngOnDestroy(): void {
-    if(this.memberService.editMode()){
+    if (this.memberService.editMode()) {
       this.memberService.editMode.set(false);
     }
-    }
+  }
 }
