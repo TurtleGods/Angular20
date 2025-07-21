de436518cd42bcce49905723e01c4f01659051b8
Turtlegods <strollist@gmail.com>
2025-07-21 13:25:02 +0800

    建立uploadPhoto 區域的功能

--- src/features/members/member-photos/member-photos.ts
+++ src/features/members/member-photos/member-photos.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -8,24 +8,30 @@ import { ImageUpload } from '../../../shared/image-upload/image-upload';
 
 @Component({
   selector: 'app-member-photos',
-  imports: [AsyncPipe,ImageUpload],
+  imports: [AsyncPipe, ImageUpload],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css'
 })
-export class MemberPhotos {
+export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
-  protected photo$?: Observable<Photo[]>;
-  constructor() {
+  protected photo = signal<Photo[]>([]);
+
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
     if (memberId) {
-      this.photo$ = this.memberService.getMemberPhotos(memberId);
+      this.memberService.getMemberPhotos(memberId).subscribe(
+        {
+          next: photos => {
+            this.photo.set(photos);
+          }
+        });
     }
   }
 
-  get photoMocks(){
-    return Array.from({length:20},(_,i)=>({
-      url:'/user.png'
+  get photoMocks() {
+    return Array.from({ length: 20 }, (_, i) => ({
+      url: '/user.png'
     }))
   }
 }
