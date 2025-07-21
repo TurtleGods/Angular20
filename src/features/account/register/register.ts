f1e5e5f2e191bc208eb686041a6d9c19da8e6fae
Turtlegods <strollist@gmail.com>
2025-07-21 17:11:15 +0800

    初步建立formGroup register

--- src/features/account/register/register.ts
+++ src/features/account/register/register.ts
@@ -1,31 +1,39 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/User';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
-  cancelRegister = output<boolean>();
+export class Register implements OnInit {
   private accountService = inject(AccountService);
-  protected creds ={} as RegisterCreds;
-
-  register(){
-    this.accountService.register(this.creds).subscribe({
-      next:response=>{
-        console.log(response);
-        this.cancel();
-      },
-      error:error=>console.log(error)
-    })
-    console.log(this.creds);
+  cancelRegister = output<boolean>();
+  protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+  
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  
+  register() {
+    console.log(this.registerForm.value);
   }
 
-  cancel(){
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl(),
+      displayName: new FormControl(),
+      confirmPassword: new FormControl(),
+    })
+  }
+
+  cancel() {
     this.cancelRegister.emit(false);
-    }
+  }
 }
