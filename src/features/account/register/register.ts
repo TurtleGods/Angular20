427af1d42abe08123ce1020b89cf8cc28c237b0e
Turtlegods <strollist@gmail.com>
2025-07-22 14:03:18 +0800

    加入register Step 畫面 同時使用fromGroup的功能

--- src/features/account/register/register.ts
+++ src/features/account/register/register.ts
@@ -1,5 +1,5 @@
-import { Component, inject, input, OnInit, output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Component, inject, output, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds } from '../../../types/User';
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
@@ -11,39 +11,58 @@ import { TextInput } from "../../../shared/text-input/text-input";
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register implements OnInit {
+export class Register  {
   private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
-  protected registerForm: FormGroup = new FormGroup({});
-  
-  ngOnInit(): void {
-    this.initializeForm();
+  protected credentialsForm: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
+  constructor() {
+    this.credentialsForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      displayName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+
+    this.profileForm = this.fb.group({
+      gender: ['', [Validators.required]],
+      dateOfBirth: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      country: ['', [Validators.required]]
+    });
+
+    this.credentialsForm.controls['password'].valueChanges.subscribe(() => {
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+  nextStep(){
+    if(this.credentialsForm.valid) {
+      this.currentStep.update(prevstep=>prevstep+1);
+      return;
+    }
+  }
+  prevStep(){
+    this.currentStep.update(prevstep=>prevstep-1);
   }
 
   register() {
-    console.log(this.registerForm.value);
+    if(this.profileForm.valid&&this.credentialsForm.valid){
+      const formData = {...this.credentialsForm.value, ...this.profileForm.value};
+      console.log('Form data: ', formData);
+    }
   }
 
-  initializeForm() {
-    this.registerForm = new FormGroup({
-      email: new FormControl('',[Validators.required,Validators.email]),
-      displayName: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
-      confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')]),
-    });
-    this.registerForm.controls['password'].valueChanges.subscribe(() => {
-      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
-    });
-  }
-
-  matchValues(matchTo:string):ValidatorFn{
-    return (control:AbstractControl):ValidationErrors|null=>{
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
       const parent = control.parent;
-      if(!parent) return null;
+      if (!parent) return null;
 
       const matchValue = parent.get(matchTo)?.value;
-      return control.value==matchValue?null:{passwordMismatch:true};
+      return control.value == matchValue ? null : { passwordMismatch: true };
     }
   }
 
