bd7abd11b3860ff76ebfe35ae586bd88c9c9a3f2
Turtlegods <strollist@gmail.com>
2025-08-03 00:38:57 +0800

    建立memberMessage畫面初版

--- src/core/services/message-service.ts
+++ src/core/services/message-service.ts
@@ -14,10 +14,14 @@ export class MessageService {
   getMessages(container: string, pageNumber: number, pageSize: number) {
     let params = new HttpParams();
 
-    params = params.append('pageNumber',pageNumber);
-    params = params.append('pageSize',pageSize);
-    params = params.append('container',container);
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+    params = params.append('container', container);
 
-    return this.http.get<PaginatedResult<Message>>(this.baseUrl+'messages',{params});
+    return this.http.get<PaginatedResult<Message>>(this.baseUrl + 'messages', { params });
+  }
+
+  getMessageThread(memberId: string) {
+    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + memberId);
   }
 }
