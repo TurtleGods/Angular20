fd85cbf4bb3a43cc7c07e50500f6f47f0c1716d5
Turtlegods <strollist@gmail.com>
2025-08-06 11:36:38 +0800

    建立client 端signalR連線 現在可以連線成功了

--- src/core/services/account-service.ts
+++ src/core/services/account-service.ts
@@ -4,6 +4,8 @@ import { RegisterCreds, User } from '../../types/User';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import LikesService from './likes-service';
+import { PresenceService } from './presence-service';
+import { HubConnectionState } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,10 @@ import LikesService from './likes-service';
 export class AccountService {
   private http = inject(HttpClient);
   private likeService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   currentUser = signal<User | null>(null);
 
-  baseUrl = environment.apiUrl;
+  private baseUrl = environment.apiUrl;
 
   register(creds: RegisterCreds) {
     return this.http.post<User>(this.baseUrl + 'account/register', creds,{withCredentials:true}).pipe(
@@ -55,6 +58,9 @@ export class AccountService {
     user.roles = this.getRolesFromToken(user);
     this.currentUser.set(user);
     this.likeService.getLikeIds();
+    if(this.presenceService.hubConnection?.state !== HubConnectionState.Connected){
+      this.presenceService.createHubConnection(user);
+    }
   }
 
   logout() {
